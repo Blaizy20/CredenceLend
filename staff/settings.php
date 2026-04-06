5c39e9544a5dac54bbf25df582b7938c00b91320
Blaizy20 <johnsenenestrella@gmail.com>
2026-04-07 06:19:00 +0800

    Update settings.php

--- staff/settings.php
+++ staff/settings.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../includes/auth.php';
 require_login();
 require_permission('view_settings');
 
-if (!current_tenant_id()) {
+if (!current_tenant_id() && !is_global_super_admin_view()) {
     $_SESSION['return_url'] = APP_BASE . '/staff/settings.php';
     header('Location: ' . APP_BASE . '/staff/select_tenant.php');
     exit;
